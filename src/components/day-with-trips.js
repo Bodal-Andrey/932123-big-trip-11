51feb8aae82f1322072b9166e0849dbe34427d1d
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-04-27 13:35:41 +0500

    Исправил ошибки

--- src/components/day-with-trips.js
+++ src/components/day-with-trips.js
@@ -1,31 +1,20 @@
 import AbstractComponent from "./abstract-component.js";
 
 const createDayWithTripsTemplate = (date, index) => {
-  if (date && index) {
-    const dateItem = date.slice(4, 10);
+  const dateVariant = date ? date : ``;
+  const indexVariant = index ? index : ``;
+  const dateItem = dateVariant.slice(4, 10);
 
-    return (
-      `<li class="trip-days__item  day">
+  return (
+    `<li class="trip-days__item  day">
       <div class="day__info">
-        <span class="day__counter">${index}</span>
+        <span class="day__counter">${indexVariant}</span>
         <time class="day__date" datetime="2019-03-18">${dateItem}</time>
       </div>
   
       <ul class="trip-events__list"></ul>
     </li>`
-    );
-  } else {
-    return (
-      `<li class="trip-days__item  day">
-      <div class="day__info">
-        <span class="day__counter"></span>
-        <time class="day__date" datetime="2019-03-18"></time>
-      </div>
-  
-      <ul class="trip-events__list"></ul>
-    </li>`
-    );
-  }
+  );
 };
 
 export default class DayWithTrips extends AbstractComponent {
